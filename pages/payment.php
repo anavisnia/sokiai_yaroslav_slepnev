f6f0291b3e18d24b458f4c9a236434fb4970f547
Ana Višnevskaja <anavisnia@gmail.com>
2021-05-19 17:46:03 +0300

    paument page

---
+++ pages/payment.php
@@ -0,0 +1,26 @@
+<?php ?>
+
+<!DOCTYPE html>
+<html lang="lt">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pirkti Pamoka</title>
+</head>
+<body>
+    <div class="payment__container">
+        <form action="" method="post">
+            <label for="name">Name:</label>
+            <input type="text" name="name">
+            <label for="surname">Surname:</label>
+            <input type="text" name="surname">
+            <button type="submit">Pirkti</button>
+        </form>
+        <div class="payment__system">
+            placeholder
+        </div>
+    </div>
+    
+</body>
+</html>
